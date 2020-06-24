5643369273fa3c1be4128705b95b79e8baed1737
AtumaKen <atumaken@gmail.comgit config --global user.name AtumaKengit commitgit statusgit statusgitgit config --global user.email atumaken@gmail.com>
2020-06-24 14:18:16 +0100

    added image placeholder

--- db_main.py
+++ db_main.py
@@ -8,6 +8,25 @@ import os
 import shutil
 import db_config
 
+
+def on_tab_selected(event):
+    selected_tab = event.widget.select()
+    tab_text = event.widget.tab(selected_tab, "text")
+
+    if tab_text == "All Records":
+        print("All records tab selected")
+    if tab_text == "Add New Record":
+        print("Add new records tab selected")
+
+def load_database_results():
+    return
+
+
+file_name = "default.png"
+path = db_config.PHOTO_DIRECTORY + file_name
+rows = None
+num_of_rows = None
+
 form = tk.Tk()
 form.title("Database Form")
 form.geometry("500x280")
@@ -16,10 +35,12 @@ tab_parent = ttk.Notebook(form)
 tab1 = ttk.Frame(tab_parent)
 tab2 = ttk.Frame(tab_parent)
 
+tab_parent.bind("<<NotebookTabChanged>>", on_tab_selected)
+
 tab_parent.add(tab1, text="All Records")
 tab_parent.add(tab2, text="Add New Record")
 
-#ADDING WIDGETS FOR TAB ONE
+# ADDING WIDGETS FOR TAB ONE
 firstLabelTabOne = tk.Label(tab1, text="First name:")
 familyLabelTabOne = tk.Label(tab1, text="Family Name:")
 jobLabelTabOne = tk.Label(tab1, text="Job Title:")
@@ -28,12 +49,14 @@ firstEntryTabOne = tk.Entry(tab1)
 familyEntryTabOne = tk.Entry(tab1)
 jobEntryTabOne = tk.Entry(tab1)
 
-imgLabelTabOne = tk.Label(tab1)
+openImageTabOne = Image.open(path)
+imgTabOne = ImageTk.PhotoImage(openImageTabOne)
+imgLabelTabOne = tk.Label(tab1, image=imgTabOne)
 
 buttonForward = tk.Button(tab1, text="Forward")
 buttonBack = tk.Button(tab1, text="Back")
 
-#ADD WIDGETS TO GRID ON TAB
+# ADD WIDGETS TO GRID ON TAB
 firstLabelTabOne.grid(row=0, column=0, padx=15, pady=15)
 firstEntryTabOne.grid(row=0, column=1, padx=15, pady=15)
 
@@ -45,7 +68,39 @@ jobEntryTabOne.grid(row=2, column=1, padx=15, pady=15)
 
 imgLabelTabOne.grid(row=0, column=2, rowspan=3, padx=15, pady=15)
 
-#ADDING WIDGETS FOR TAB2
+buttonBack.grid(row=3, column=0, padx=15, pady=15)
+buttonForward.grid(row=3, column=2, padx=15, pady=15)
+
+# ADDING WIDGETS FOR TAB2
+firstLabelTabTwo = tk.Label(tab2, text="FirstName:")
+familyLabelTabTwo = tk.Label(tab2, text="Family Name:")
+jobLabelTabTwo = tk.Label(tab2, text="Job Title:")
+
+firstEntryTabTwo = tk.Entry(tab2)
+familyEntryTabTwo = tk.Entry(tab2)
+jobEntryTabTwo = tk.Entry(tab2)
+
+openImageTwo = Image.open(path)
+imageTabTwo = ImageTk.PhotoImage(openImageTabOne)
+imageLabelTabTwo = tk.Label(tab2, image=imageTabTwo)
+
+buttonCommit = tk.Button(tab2, text="Add Record To Database")
+buttonAddImage = tk.Button(tab2, text="Add Image")
+
+# ADD WIDGETS ON TAB TWO
+firstLabelTabTwo.grid(row=0, column=0, padx=15, pady=15)
+firstEntryTabTwo.grid(row=0, column=1, padx=15, pady=15)
+
+familyLabelTabTwo.grid(row=1, column=0, padx=15, pady=15)
+familyEntryTabTwo.grid(row=1, column=1, padx=15, pady=15)
+
+jobLabelTabTwo.grid(row=2, column=0, padx=15, pady=15)
+jobEntryTabTwo.grid(row=2, column=1, padx=15, pady=15)
+
+buttonCommit.grid(row=4, column=1, padx=15, pady=15)
+buttonAddImage.grid(row=4, column=2, padx=15, pady=15)
+
+imageLabelTabTwo.grid(row=0, column=2, rowspan= 3, padx=15, pady=15)
 
 tab_parent.pack(expand=1, fill="both")
 
